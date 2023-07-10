5cc2315289a3bc7efaa5ea7cbabc5427000ecf80
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-09 21:57:44 -0400

    Calculator Lab

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(a, b) {
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+    return a+b
+}
+
+function subtract(a, b){
+    return a-b
+}
+
+function multiply(a, b){
+    return a*b
+}
+
+function divide(a, b){
+    return a/b
+}
+
+function add(a, b){
+    var result = a+b;
+    return result
+}
+
+function increment(a){
+    var result = a+ 1
+    return result
+}
+
+function decrement(a){
+    var result = a - 1
+    return result
+}
+
+function makeInt(n){
+    return n
+}
+
+function makeInt(base10){
+    return parseInt(base10, 10)
+    
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+
+}
+
+
+
+
+
+
